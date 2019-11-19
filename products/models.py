4dc8fb5091838366c23ace64b7f3ca82e184d616
Szymon Słomiński <simon.slominski@gmail.com>
2019-11-19 11:18:25 +0100

    add basic Product Model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        ordering = ('name',)
+
+    """
+    Remember to add 'comma' after ordering element. Otherwise you will get an error like below
+    ERRORS: 'ordering' must be a tuple or list (even if you want to order by only one field).
+    """
+
+    def __str__(self):
+        return self.name
