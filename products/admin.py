79a448df5fe80d6715c925fbb553c97f3491251e
Szymon Słomiński <simon.slominski@gmail.com>
2019-11-20 12:51:52 +0100

    update admin.py by adding ProductAdmin

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Product
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'slug']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Product, ProductAdmin)
