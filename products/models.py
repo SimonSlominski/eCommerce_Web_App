751c7e6f54f3c66f7f3835920d96fa313b70d2c5
Szymon Słomiński <simon.slominski@gmail.com>
2019-11-20 12:47:17 +0100

    update models by adding slug, stock, available fields

--- products/models.py
+++ products/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 
 class Product(models.Model):
-    name        = models.CharField(max_length=200)
+    name        = models.CharField(max_length=200, db_index=True)
+    slug        = models.SlugField(max_length=200, db_index=True, null=True)
     description = models.TextField(blank=True)
     price       = models.DecimalField(max_digits=10, decimal_places=2)
     image       = models.ImageField(upload_to='products/', blank=True, null=True)
+    stock       = models.PositiveIntegerField()
+    available   = models.BooleanField(default=True)
     created     = models.DateTimeField(auto_now_add=True, null=True)
     updated     = models.DateTimeField(auto_now=True, null=True)
 
