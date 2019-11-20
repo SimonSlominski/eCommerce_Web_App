09ad1e101d0547e1611a81e46994f5d08c91dad1
Szymon Słomiński <simon.slominski@gmail.com>
2019-11-20 12:53:01 +0100

    update admin.py by adding class Meta to ProductAdmin

--- products/admin.py
+++ products/admin.py
@@ -7,4 +7,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['available', 'created', 'updated']
     list_editable = ['price', 'stock', 'available']
     prepopulated_fields = {'slug': ('name',)}
+
+    class Meta:
+        model = Product
+        
 admin.site.register(Product, ProductAdmin)
