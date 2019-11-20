3a88a045b681134a0e445578e1427c6dd6645d86
Szymon Słomiński <simon.slominski@gmail.com>
2019-11-20 10:47:28 +0100

    add Products ListView; creating CRUD step 1

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
+from django.views.generic.list import ListView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+
+class ProductListView(ListView):
+    model = Product
+    queryset = Product.objects.all()
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     return context
+
