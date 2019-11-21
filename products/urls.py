b241a0d9fcc15e3cbbfab460d021b8310dde3494
Szymon Słomiński <simon.slominski@gmail.com>
2019-11-21 12:13:36 +0100

    add products app urls and include in project's urls

---
+++ products/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import ProductListView, ProductDetailView
+
+urlpatterns = [
+    path('list', ProductListView.as_view(), name='product-list'),
+    path('product/<slug:slug>/', ProductDetailView.as_view(), name='product-detail'),
+]
+
