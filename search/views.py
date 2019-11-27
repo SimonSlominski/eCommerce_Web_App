6b4829a7b2e7015335eacbc27a9de20f3558c1f6
Szymon Słomiński <simon.slominski@gmail.com>
2019-11-27 12:41:42 +0100

    update views.py by adding SearchProductView + add template

--- search/views.py
+++ search/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views.generic import ListView
 from django.shortcuts import render
 
@@ -6,13 +7,18 @@ from products.models import Product
 
 class SearchProductView(ListView):
     model = Product
+    template_name = 'search/view.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(SearchProductView, self).get_context_data(*args, **kwargs)
+        query = self.request.GET.get('q')
+        context['query'] = query
+        return context
 
     def get_queryset(self, *args, **kwargs):
         request = self.request
-        query = request.GET.get('q')
+        query = request.GET.get('q', None)
         if query is not None:
-            return Product.objects.filter(title_icontains=query)
-        return Product.objects.none()
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(SearchProductView, self).get_context_data(*args, **kwargs)
-    #     return context
+            lookups = Q(name__icontains=query) | Q(description__icontains=query)
+            return Product.objects.filter(lookups).distinct()
+
