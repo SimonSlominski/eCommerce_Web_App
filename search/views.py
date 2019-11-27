fc4692c710a9aa6b077616697aa41309e56ec0f3
Szymon Słomiński <simon.slominski@gmail.com>
2019-11-27 10:59:39 +0100

    add SearchProductView

--- search/views.py
+++ search/views.py
@@ -1,3 +1,18 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 
-# Create your views here.
+from products.models import Product
+
+
+class SearchProductView(ListView):
+    model = Product
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        query = request.GET.get('q')
+        if query is not None:
+            return Product.objects.filter(title_icontains=query)
+        return Product.objects.none()
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(SearchProductView, self).get_context_data(*args, **kwargs)
+    #     return context
